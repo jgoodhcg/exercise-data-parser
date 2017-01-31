5e998e36d5e4f452d321650b6901936f0244b77a
jgoodhcg <jgoodhcg@gmail.com>
2017-01-31 06:53:58 -0500

    working slurp

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to exercise-data-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/exercise_data_parser/core.clj
@@ -0,0 +1,20 @@
+(ns exercise-data-parser.core
+  (:gen-class)
+  (:require [clojure-csv.core :refer [parse-csv]]
+            [clj-time.core :as time]
+            [clj-time.coerce :as coerce]
+            [clojure.string :refer [split]]
+            [clojure.data.json :as json]))
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(let [csv-str (slurp "/exercise-data-parser/resources/exercise-data-2016.csv")
+      csv-vec (parse-csv csv-str)
+      keys (first csv-vec)]
+  keys)
+
+
